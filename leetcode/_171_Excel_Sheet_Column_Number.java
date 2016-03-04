07402c92dcbc21edbeb02faa5579c7cb905a3abb
sunxiaoyang <sxy.winner@gmail.com>
2016-03-04 21:29:20 +0800

    _171_Excel_Sheet_Column_Number

--- leetcode/_171_Excel_Sheet_Column_Number.java
+++ leetcode/_171_Excel_Sheet_Column_Number.java
@@ -1,5 +1,17 @@
 /**
- * Created by boy on 16-3-4.
+ * @author sunxiaoyang
  */
 public class _171_Excel_Sheet_Column_Number {
+    public int titleToNumber(String s) {
+        int sum = 0;
+        for (char i : s.toUpperCase().toCharArray()) {
+            sum = sum * 26 + (i - 'A' + 1);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        _171_Excel_Sheet_Column_Number a = new _171_Excel_Sheet_Column_Number();
+        System.out.println(a.titleToNumber("z"));
+    }
 }
