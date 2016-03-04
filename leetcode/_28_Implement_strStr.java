f3fe3b6aa153cc942fb5e1667ef4e7e52fdfdabd
sunxiaoyang <sxy.winner@gmail.com>
2016-03-04 21:13:17 +0800

    28. Implement strStr()

--- leetcode/_28_Implement_strStr.java
+++ leetcode/_28_Implement_strStr.java
@@ -1,5 +1,22 @@
 /**
- * Created by boy on 16-3-4.
+ * Note:
+ * strStr("", "") = 0
+ *
+ * @author sunxiaoyang
  */
 public class _28_Implement_strStr {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        if (haystack.isEmpty()) {
+            return -1;
+        }
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        _28_Implement_strStr a = new _28_Implement_strStr();
+        System.out.println(a.strStr("213", "1"));
+    }
 }
