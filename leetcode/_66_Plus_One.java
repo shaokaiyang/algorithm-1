3ed03b297f2809be90a41537417439b48769e88d
sunxiaoyang <sxy.winner@gmail.com>
2016-03-04 21:06:03 +0800

    66. Plus One

---
+++ leetcode/_66_Plus_One.java
@@ -0,0 +1,34 @@
+/**
+ * Created by boy on 16-3-4.
+ */
+public class _66_Plus_One {
+    public int[] plusOne(int[] digits) {
+        digits[digits.length - 1] += 1;
+
+        for (int i = digits.length - 1; i > 0; i--) {
+            digits[i - 1] += digits[i] / 10;
+            digits[i] %= 10;
+        }
+
+        if (digits[0] > 9) {
+            int[] result = new int[digits.length + 1];
+            for (int i = 0; i < digits.length; i++) {
+                result[i + 1] = digits[i];
+            }
+            result[0] = result[1] / 10;
+            result[1] %= 10;
+            return result;
+        } else {
+            return digits;
+        }
+    }
+
+    public static void main(String[] args) {
+        _66_Plus_One a = new _66_Plus_One();
+        int[] in = {9, 9, 9};
+        for (int i : a.plusOne(in)) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
