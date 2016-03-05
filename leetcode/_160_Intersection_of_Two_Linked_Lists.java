2081247caae9652e7213355fdd7d4299ef18e91b
sunxiaoyang <sxy.winner@gmail.com>
2016-03-05 16:35:41 +0800

    160. Intersection of Two Linked Lists

---
+++ leetcode/_160_Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,67 @@
+/**
+ * Notes:
+ * code should preferably run in O(n) time
+ *
+ * @author sunxiaoyang
+ */
+public class _160_Intersection_of_Two_Linked_Lists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // two lists are null
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        // two lists are acutally one
+        if (headA == headB) {
+            return headA;
+        }
+
+        int sumA = 0, sumB = 0;
+        for (ListNode temp = headA; temp != null; temp = temp.next) {
+            sumA++;
+        }
+        for (ListNode temp = headB; temp != null; temp = temp.next) {
+            sumB++;
+        }
+
+        int be = sumA - sumB;
+        if (sumA < sumB) {
+            ListNode temp = headA;
+            headA = headB;
+            headB = temp;
+            be = sumB - sumA;
+        }
+        while (be-- != 0) {
+            headA = headA.next;
+        }
+
+        while (headA != null) {
+            if (headA == headB) {
+                return headA;
+            } else {
+                headA = headA.next;
+                headB = headB.next;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
