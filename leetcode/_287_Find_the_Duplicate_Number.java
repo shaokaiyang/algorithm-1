f4deaf13d73ecf735b103db2850eee9c7ed71305
sunxiaoyang <sxy.winner@gmail.com>
2016-03-19 10:49:27 +0800

    _287_Find_the_Duplicate_Number

--- leetcode/_287_Find_the_Duplicate_Number.java
+++ leetcode/_287_Find_the_Duplicate_Number.java
@@ -1,5 +1,33 @@
 /**
- * Created by boy on 16-3-6.
+ * Notes:
+ * [8,1,1,1,2,7,4,3,1,1]
+ * [1,4,4,2,4]
+ *
+ * @author sunxiaoyang
  */
 public class _287_Find_the_Duplicate_Number {
+    public int findDuplicate(int[] nums) {
+        int min = 1, max = nums.length - 1;
+
+        while (min < max) {
+            int mid = (min + max) / 2;
+            int count = 0;
+            for (int i : nums) {
+                count += i >= min && i <= max && i > mid ? 1 : 0;
+            }
+            if (count > (max - min + 1) / 2) {
+                min = mid + 1;
+            } else {
+                max = mid;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        _287_Find_the_Duplicate_Number a = new _287_Find_the_Duplicate_Number();
+        int[] in = {8, 1, 1, 1, 2, 7, 4, 3, 1, 1};
+//        int[] in = {1, 4, 4, 2, 4};
+        System.out.println(a.findDuplicate(in));
+    }
 }
