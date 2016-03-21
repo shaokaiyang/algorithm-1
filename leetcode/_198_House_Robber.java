8fb530ce7a939830526052513714e2713f0c38f2
sunxiaoyang <sxy.winner@gmail.com>
2016-03-21 18:52:28 +0800

    198. House Robber

---
+++ leetcode/_198_House_Robber.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+/**
+ * Notes:
+ * 动态规划
+ *
+ * @author sunxiaoyang
+ */
+public class _198_House_Robber {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        _198_House_Robber a = new _198_House_Robber();
+        int[] in = new int[]{183, 219, 57, 193, 94, 233, 202, 154, 65, 240, 97, 234, 100, 249, 186, 66, 90, 238, 168, 128, 177, 235, 50, 81, 185, 165, 217, 207, 88, 80, 112, 78, 135, 62, 228, 247, 211};
+        System.out.println(a.rob(in));
+    }
+}
