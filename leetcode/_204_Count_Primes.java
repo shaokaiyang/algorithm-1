bfa210879af431e9091d3535c0c51592a7988f94
sunxiaoyang <sxy.winner@gmail.com>
2016-03-28 19:49:32 +0800

    204. Count Primes

--- leetcode/_204_Count_Primes.java
+++ leetcode/_204_Count_Primes.java
@@ -7,10 +7,10 @@ public class _204_Count_Primes {
         int result = 0;
         for (int i = 0; i < n; i++) re[i] = 1;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i*i < n; i++) {
             if (re[i] != 0) {
                 re[i] = 1;
-                for (int j = 2; j * i < n; j++) re[j * i] = 0;
+                for (int j = i; j * i < n; j++) re[j * i] = 0;
             }
         }
         for (int i = 2; i < n; i++) result += re[i];
@@ -20,6 +20,6 @@ public class _204_Count_Primes {
 
     public static void main(String[] args) {
         _204_Count_Primes a = new _204_Count_Primes();
-        System.out.println(a.countPrimes(10));
+        System.out.println(a.countPrimes(1500000));
     }
 }
