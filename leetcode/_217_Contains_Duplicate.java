1a14fa6f91bb7bcb9f3cf05108b5f1b81abd7683
sunxiaoyang <sxy.winner@gmail.com>
2016-05-05 18:09:05 +0800

    _217_Contains_Duplicate

---
+++ leetcode/_217_Contains_Duplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author sunxiaoyang
+ */
+public class _217_Contains_Duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<Integer>();
+        for (int i : nums) {
+            s.add(i);
+        }
+        return s.size() != nums.length;
+    }
+}
