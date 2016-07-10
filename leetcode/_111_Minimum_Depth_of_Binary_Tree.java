869ec9b1e3151a90330698c044bb50272782a875
sunxiaoyang <sxy.winner@gmail.com>
2016-07-10 16:46:02 +0800

    _111_Minimum_Depth_of_Binary_Tree

---
+++ leetcode/_111_Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+
+/**
+ * @author sunxiaoyang
+ */
+public class _111_Minimum_Depth_of_Binary_Tree {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null)
+            return 1;
+        if (root.left == null)
+            return 1 + minDepth(root.right);
+        if (root.right == null)
+            return 1 + minDepth(root.left);
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
