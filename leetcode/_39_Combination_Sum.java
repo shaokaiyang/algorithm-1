dbe37b8159b9705317623f979cc0c014a52b95a6
sunxiaoyang <sxy.winner@gmail.com>
2016-07-14 15:30:13 +0800

    _39_Combination_Sum

--- leetcode/_39_Combination_Sum.java
+++ leetcode/_39_Combination_Sum.java
@@ -7,27 +7,39 @@ public class _39_Combination_Sum {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
 
-        for (int i : candidates) {
-            if (i < target) {
-                List<List<Integer>> temp = combinationSum(candidates, target - i);
+        for (int i = 0; i < candidates.length; i++) {
+            if (i > 0 && candidates[i] == candidates[i - 1]) continue;
+
+            List<List<Integer>> temp = null;
+            if (candidates[i] < target) {
+                temp = combinationSum(candidates, target - candidates[i]);
+            } else if (candidates[i] == target) {
+                temp = new ArrayList<>();
+                List<Integer> l = new ArrayList<>();
+                temp.add(l);
+            }
+            if (temp != null)
                 for (List<Integer> l : temp) {
-                    l.add(i);
+                    l.add(candidates[i]);
                     Collections.sort(l);
                     if (!res.contains(l))
                         res.add(l);
                 }
-            } else if (i == target) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(i);
-                res.add(temp);
-            }
+            else
+                break;
         }
         return res;
     }
 
     public static void main(String[] args) {
         _39_Combination_Sum a = new _39_Combination_Sum();
-
+        int[] in = {92, 71, 89, 74, 102, 91, 70, 119, 86, 116, 114, 106, 80, 81, 115, 99, 117, 93, 76, 77, 111, 110, 75, 104, 95, 112, 94, 73};
+        for (List<Integer> i : a.combinationSum(in, 310)) {
+            for (Integer j : i)
+                System.out.print(j + " ");
+            System.out.println();
+        }
     }
 }
