fccadb52fc7ef5d1087ecb532d82c530de2ebc63
sunxiaoyang <sxy.winner@gmail.com>
2016-07-14 17:55:36 +0800

    _3_Longest_Substring_Without_Repeating_Characters

--- leetcode/_3_Longest_Substring_Without_Repeating_Characters.java
+++ leetcode/_3_Longest_Substring_Without_Repeating_Characters.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+
 /**
- * Created by boy on 16-7-14.
+ * @author sunxiaoyang
  */
 public class _3_Longest_Substring_Without_Repeating_Characters {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> temp = new HashMap<>();
+        char[] v = s.toCharArray();
+        int res = 0, cur = 0, last = -1;
+
+        for (int i = 0; i < v.length; i++) {
+            if (temp.get(v[i]) != null && temp.get(v[i]) > last) {
+                res = Math.max(res, cur);
+                cur = i - temp.get(v[i]) - 1;
+                last = temp.get(v[i]);
+            }
+            temp.put(v[i], i);
+            cur++;
+        }
+        return Math.max(res, cur);
+    }
+
+    public static void main(String[] args) {
+        _3_Longest_Substring_Without_Repeating_Characters a = new _3_Longest_Substring_Without_Repeating_Characters();
+        System.out.println(a.lengthOfLongestSubstring("abcabcbb"));
+    }
 }
