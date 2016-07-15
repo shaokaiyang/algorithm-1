51bacb46a91b72512a43e602f058f8724290bc38
sunxiaoyang <sxy.winner@gmail.com>
2016-07-15 21:35:45 +0800

    _48_Rotate_Image

---
+++ leetcode/_48_Rotate_Image.java
@@ -0,0 +1,17 @@
+/**
+ * @author sunxiaoyang
+ */
+public class _48_Rotate_Image {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = t;
+            }
+        }
+    }
+}
