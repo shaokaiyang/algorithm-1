fdc099f128f5e99ccd83275431da7e076521c35f
sunxiaoyang <sxy.winner@gmail.com>
2017-03-16 21:32:28 +0800

    189. Rotate Array

--- leetcode/_189_Rotate_Array.java
+++ leetcode/_189_Rotate_Array.java
@@ -5,7 +5,8 @@
  * @author sunxiaoyang
  */
 public class _189_Rotate_Array {
-    public void rotate(int[] nums, int k) {
+    // 2016-03-05
+    public void rotate_1(int[] nums, int k) {
         int len = nums.length - 1;
         k %= len + 1;
 
@@ -18,10 +19,33 @@ public class _189_Rotate_Array {
         }
     }
 
+    // 2017-03-16
+    // 利用下标变化。0 移至 (0+k), (0+k) 移至 (0+k+k),..., ((i+k)%len) 移至 ((i+k)%len+k)%len ...
+    public void rotate(int[] nums, int k) {
+        int foot = 0;
+        int swap = nums[foot];
+        int len = nums.length;
+        boolean flag[] = new boolean[nums.length]; // 'false': nums[i] is not visited, 'true': visited
+        while (len-- > 0) {
+            foot = (foot + k) % nums.length;
+            if (flag[foot]) { // jump to another group
+                foot++;
+                swap = nums[foot];
+                len++;
+                continue;
+            }
+            flag[foot] = true;
+            int temp = nums[foot];
+            nums[foot] = swap;
+            swap = temp;
+        }
+    }
+
     public static void main(String[] args) {
         _189_Rotate_Array a = new _189_Rotate_Array();
-        int[] in = new int[]{1, 2, 3, 4, 5, 6, 7};
-        a.rotate(in, 3);
+        int[] in = new int[]{1, 2, 3, 4, 5, 6};
+//        int[] in = new int[]{1, 2};
+        a.rotate(in, 4);
         for (int i : in) {
             System.out.print(i + " ");
         }
