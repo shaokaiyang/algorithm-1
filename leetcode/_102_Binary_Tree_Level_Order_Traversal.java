4dc7206109d3569c7a9bb5fb56dfe7503b15b556
sunxiaoyang <sxy.winner@gmail.com>
2017-03-18 19:44:37 +0800

    102. Binary Tree Level Order Traversal

--- leetcode/_102_Binary_Tree_Level_Order_Traversal.java
+++ leetcode/_102_Binary_Tree_Level_Order_Traversal.java
@@ -1,36 +1,13 @@
+import java.util.ArrayDeque;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Queue;
 
 /**
  * @author sunxiaoyang
  */
 
 public class _102_Binary_Tree_Level_Order_Traversal {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<TreeNode> s = new ArrayList<>();
-        List<List<Integer>> l = new ArrayList<>();
-
-        if (root == null) return l;
-        s.add(root);
-
-        while (!s.isEmpty()) {
-            List<Integer> li = new ArrayList<>();
-            List<TreeNode> q = new ArrayList<>();
-            for (TreeNode i : s) {
-                li.add(i.val);
-                q.add(i);
-            }
-            s.clear();
-            if (!li.isEmpty()) l.add(li);
-
-            for (TreeNode i : q) {
-                if (i.left != null) s.add(i.left);
-                if (i.right != null) s.add(i.right);
-            }
-        }
-        return l;
-    }
-
     /**
      * Definition for a binary tree node.
      */
@@ -43,4 +20,53 @@ public class _102_Binary_Tree_Level_Order_Traversal {
             val = x;
         }
     }
+
+    // 2016-05-08
+    public List<List<Integer>> levelOrder_1(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        List<TreeNode> queue = new ArrayList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level_result = new ArrayList<>();
+            for (TreeNode i : queue) {
+                level_result.add(i.val);
+            }
+            if (!level_result.isEmpty()) result.add(level_result);
+
+            List<TreeNode> tmp = new ArrayList<>(queue);
+            queue.clear();
+
+            for (TreeNode i : tmp) {
+                if (i.left != null) queue.add(i.left);
+                if (i.right != null) queue.add(i.right);
+            }
+        }
+        return result;
+    }
+
+    // 2017-03-18
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int len = queue.size();
+            while (len-- > 0) {
+                TreeNode tmp = queue.poll();
+                level.add(tmp.val);
+                if (tmp.left != null) queue.add(tmp.left);
+                if (tmp.right != null) queue.add(tmp.right);
+            }
+            if (!level.isEmpty()) result.add(level);
+        }
+        return result;
+    }
+
 }
