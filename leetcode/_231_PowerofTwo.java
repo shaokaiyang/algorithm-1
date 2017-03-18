36d3b2a48cd6424643fabebc78137139dc52ad00
sunxiaoyang <sxy.winner@gmail.com>
2017-03-18 20:01:21 +0800

    231. Power of Two

--- leetcode/_231_PowerofTwo.java
+++ leetcode/_231_PowerofTwo.java
@@ -1,14 +1,21 @@
 /**
  * 注意整数最高位为符号位，比对时最大为第31位为1
+ *
  * @author sunxiaoyang
  */
 
 public class _231_PowerofTwo {
-    public boolean isPowerOfTwo(int n) {
+    // 2015-10-22
+    public boolean isPowerOfTwo_1(int n) {
         for (int i = 0, t = 1; i < 31; i++) {
             if (n == t << i) return true;
         }
         return false;
+    }
+
+    // 2017-03-18
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
     }
 
     public static void main(String[] args) {
