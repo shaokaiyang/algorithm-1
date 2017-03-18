29a0e33819ca6981cb89b1899ff33408b95b84e1
sunxiaoyang <sxy.winner@gmail.com>
2017-03-18 10:39:11 +0800

    141. Linked List Cycle

---
+++ leetcode/_141_Linked_List_Cycle.java
@@ -0,0 +1,28 @@
+/**
+ * Created by sunxiaoyang on 3/18/17.
+ */
+public class _141_Linked_List_Cycle {
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+}
