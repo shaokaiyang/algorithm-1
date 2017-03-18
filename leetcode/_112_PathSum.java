ab3fd3fbeb1f73a4f1817538a3c29fb00ab2599f
sunxiaoyang <sxy.winner@gmail.com>
2017-03-18 17:12:12 +0800

    112. Path Sum

--- leetcode/_112_PathSum.java
+++ leetcode/_112_PathSum.java
@@ -2,34 +2,25 @@
  * @author sunxiaoyang
  */
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
 public class _112_PathSum {
+    /**
+     * Definition for a binary tree node.
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
     public boolean hasPathSum(TreeNode root, int sum) {
-        if (root == null) {
-            return false;
-        }
+        if (root == null) return false;
+        if (root.left == null && root.right == null && root.val == sum) return true;
 
-        if (root.left == null && root.right == null && root.val == sum) {
-            return true;
-        }
-
-        if (root.left != null && hasPathSum(root.left, sum - root.val)) {
-            return true;
-        }
-
-        if (root.right != null && hasPathSum(root.right, sum - root.val)) {
-            return true;
-        }
-        return false;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
 }
